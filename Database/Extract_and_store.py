8656e53744d286d4f63523828b770098ac62c51b
AkankshaKaple <akankshakaple@gmail.com>
2019-09-09 20:16:15 +0530

    Extract data and store to database

--- Database/Extract_and_store.py
+++ Database/Extract_and_store.py
@@ -2,14 +2,14 @@ from bs4 import BeautifulSoup
 from googlesearch import search
 import json
 from pymongo import MongoClient
-from flask import Flask
+from flask import Flask, request, jsonify
 import requests
 from bson import ObjectId
 
 app = Flask(__name__)
 client = MongoClient()
 db = client.F_Database
-Company_Name = 'Niyo Solutions'
+# Company_Name = 'Niyo Solutions'
 HTMLs = ['funding.html', 'jobs.html', 'linkedin.html', 'main.html',
          'people.html', 'crunch_base.html']
 
@@ -22,16 +22,11 @@ for html in HTMLs:
 print(html_data.keys())
 
 
-# @app.route('/get_html', methonds=['POST'])
-# def get_html():
-#
-
-
-@app.route('/jobs', methods=['GET', 'POST'])
-def jobs():
-    soup = BeautifulSoup(html_data['jobs'], features="html.parser")
-    jobs = soup.findAll(class_='component_e6bd3 expanded_80d76')
-    software_jos = [job.text for job in jobs if 'Software' in job.text]
+# Angel.co
+def jobs(html_page, reference_id):
+    soup = BeautifulSoup(html_page, features="html.parser")
+    current_jobs = soup.findAll(class_='component_e6bd3 expanded_80d76')
+    software_jos = [job.text for job in current_jobs if 'Software' in job.text]
     job_info = {}
     for index in range(len(software_jos)):
         Jobs = {}
@@ -46,26 +41,27 @@ def jobs():
                 break
         Jobs['Salary'] = job[1] + job[2]
         job_info['Job_' + str(index)] = Jobs
+    job_info['Reference_id'] = reference_id
+    db.Jobs.insert(job_info)
     return job_info
 
 
-@app.route('/company_details', methods=['GET', 'POST'])
-def company_details():
-    soup = BeautifulSoup(html_data['main'], features="html.parser")
-    dict_1 = {'Detail': soup.find_all(class_='description_f92d0')[0].text}
+# Angel.co
+def details(html_page):
+    soup = BeautifulSoup(html_page, features="html.parser")
+    detail_info = {'Detail': soup.find_all(class_='description_f92d0')[0].text}
 
     area_of_interest = []
     for i in soup.find_all(class_='styles_component__3BR-y'):
         area_of_interest.append(i.text)
 
-    dict_1['Area_of_interest'] = area_of_interest
-    detail_info = json.dumps(dict_1)
+    detail_info['Area_of_interest'] = area_of_interest
     return detail_info
 
 
-@app.route('/funding', methods=['GET', 'POST'])
-def funding():
-    soup = BeautifulSoup(html_data['funding'], features="html.parser")
+# Crunch base
+def funding(html_page, reference_id):
+    soup = BeautifulSoup(html_page, features="html.parser")
     fund = {'Total_funding': soup.find(class_='component_43375').text.split(' ')[0].replace('Funding', '')[:-1],
             'funding_rounds': soup.find(class_='component_43375').text.split(' ')[0].replace('Funding', '')[
                               -1:]}
@@ -82,12 +78,14 @@ def funding():
             fund['Round ' + round_list[j]] = i.text
             j += 1
 
+    fund['Reference_id'] = reference_id
+    db.Funding.insert(fund)
     return fund
 
 
-@app.route('/crunch_base', methods=['GET', 'POST'])
-def crunch_base():
-    soup = BeautifulSoup(html_data['crunch_base'], features="html.parser")
+# Crunch base
+def Founder(html_page, reference_id):
+    soup = BeautifulSoup(html_page, features="html.parser")
     parameters = ['Total Funding Amount', 'Number of Funding Rounds', 'Number of Lead Investors',
                   'Monthly Visits', 'Owler Estimated Revenue', 'Number of Current Team Members']
     keywords = ['Chief', 'Executive', 'Officer', 'Associate', 'President',
@@ -115,12 +113,15 @@ def crunch_base():
                 index += 1
                 break
     crunch_base_data = {'Founder': founder, 'Company_Information': Company_Information}
-    crunch_base_data = json.dumps(crunch_base_data)
+    crunch_base_data['Founder']['Reference_id'] = reference_id
+    crunch_base_data['Company_Information']['Reference_id'] = reference_id
+    db.Founder.insert(crunch_base_data['Founder'])
+    db.Company_Information.insert(crunch_base_data['Company_Information'])
     return crunch_base_data
 
 
-@app.route('/get_domain_search', methods=['GET', 'POST'])
-def get_domain_search():
+# Snov io
+def contact_person(domain_name):
     contact_info = {'domain': 'https://www.goniyo.com/',
                     'webmail': False,
                     'result': 1,
@@ -144,7 +145,6 @@ def get_domain_search():
     return contact_info
 
 
-@app.route('/print_data/<data_id>', methods=['POST', 'GET'])
 def print_data(data_id):
     print('Print data')
     Company_Name_by_document = ''
@@ -165,7 +165,6 @@ def print_data(data_id):
         data['_id'] = str(data['_id'])
         data_dict['Company_Data'] = data
         break
-
 
     # Company_Information
     cursor = db.Company_Information.find({'Reference_id': str(ref_id)})
@@ -209,105 +208,124 @@ def print_data(data_id):
         data['_id'] = str(data['_id'])
         data_dict['URLs'] = data
         break
-    print(data_dict)
-    return json.dumps(data_dict)
+    return data_dict
 
 
-@app.route('/get_urls', methods=['GET', 'POST'])
-def get_urls():
+# Google library
+def get_urls(company_name):
     sites = ['LinkedIn', 'Angel_co', 'Tech_crunch', 'Website']
     urls = {}
     for site in sites:
-        query = Company_Name + site
+        query = company_name + site
         url_generator = search(query, tld="com", num=1, stop=1, pause=2)
         for url in url_generator:
             urls[site] = url
-    return json.dumps(urls)
+    return urls
 
 
-@app.route('/save_html/<key>/<html_value>', methods=['POST'])
-def save_html(key, html_value):
-    page_name = key.split('$')[1]
-    html_files = {page_name: html_value}
-    print(html_files)
-    return json.dumps(html_files)
+def data_unavailable(data_dict, urls):
+    angel = ["Round A", "Round B", "Round c", "Round D", "Seed", "Total_funding", "funding_rounds"]
+    crunch_base = ["Area_of_interest", "Founder", "Company_Information"]
+    linked_in = ['Employee', 'Location']
+    angel_data = {}
+    crunch_base_data = {}
+    dict_2 = {}
+    linked_in_data = {}
+    key_list = []
+    for key in data_dict.keys():
+        key_list = key_list + list(data_dict[key])
+        dict_2 = {**dict_2, **data_dict[key]}
+
+    for val in key_list:
+        if val in angel:
+            angel_data[val] = dict_2[val]
+        if val in crunch_base:
+            crunch_base_data[val] = dict_2[val]
+        if val in linked_in:
+            linked_in_data[val] = dict_2[val]
+
+        crunch_base_data['url'] = urls['Tech_crunch']
+        linked_in_data['url'] = urls['LinkedIn']
+        angel_data['url'] = urls['Angel_co']
+
+    dict_1 = {'crunch_base_data': crunch_base_data, 'angel_co_data': angel_data, 'linked_in_data': linked_in_data}
+
+    return json.dumps(dict_1)
 
 
-@app.route('/database', methods=['GET', 'POST'])
-def database():
-    # Get URLs of related to company
-    res = requests.get('http://127.0.0.1:5000/get_urls')
-    urls = json.loads(res.text)
-
+@app.route('/send_urls', methods=['GET', 'POST'])
+def send_urls(urls):
     print('Check if company data already exists in database')
     cursor = db.Company_Data.find()
-    for data in cursor:
-        print(Company_Name.lower())
+    for cursor_data in cursor:
         # check if company website already exists in database
-        if urls['Website'] == data['Website']:
-            data_id = str(data['_id'])
+        if urls['Website'] == cursor_data['Website']:
+            data_id = str(cursor_data['_id'])
             print('Company already exists in database')
-            res = requests.get('http://127.0.0.1:5000/print_data/'+data_id)
-            return json.loads(res.text)
+            # Check if everything is present in the database
+            return data_unavailable(print_data(data_id), urls)
         else:
             print('Company does not exist in database')
+            return data_unavailable({}, urls)
 
-    # Contact_person
-    res = requests.get('http://127.0.0.1:5000/get_domain_search')
-    contact_info = json.loads(res.text)
 
-    # Company_detail
-    res = requests.get('http://127.0.0.1:5000/company_details')
-    company_info_ac = json.loads(res.text)
+@app.route('/company_detail', methods=['GET', 'POST'])
+def company_detail():
+    # Get URLs of related to company
+    company_name = request.form['company_name']
+    urls = get_urls(company_name)
 
-    # Company_Data
-    db.Company_Data.insert({'Website': urls['Website'],
-                            "Company_Name": contact_info['companyName'],
-                            'Area_of_interest': company_info_ac['Area_of_interest'],
-                            })
+    return send_urls(urls)
 
-    # Get primary key for the document
-    cursor = db.Company_Data.find({'Company_Name': contact_info['companyName']})
-    reference_id = ''
-    for data in cursor:
-        reference_id = str(data['_id'])
-        break
-
-    # urls_document
-    urls['Reference_id'] = reference_id
-    db.URLs.insert(urls)
-
-    # Contact_Person_document
-    db.Contact_Person.insert({'First_Name': contact_info['firstName'],
-                              'Last_Name': contact_info['lastName'],
-                              'Position': contact_info['position'],
-                              'LinkedIn_Profile': contact_info['sourcePage'],
-                              'Email': contact_info['email'],
-                              'Reference_id': reference_id
-                              })
-
-    # Jobs_document
-    res = requests.get('http://127.0.0.1:5000/jobs')
-    job = json.loads(res.text)
-    job['Reference_id'] = reference_id
-    db.Jobs.insert(job)
-
-    # Founders_document and Comp_info_document
-    res = requests.get('http://127.0.0.1:5000/crunch_base')
-    crunch_base_data = json.loads(res.text)
-    crunch_base_data['Founder']['Reference_id'] = reference_id
-    crunch_base_data['Company_Information']['Reference_id'] = reference_id
-    db.Founder.insert(crunch_base_data['Founder'])
-    db.Company_Information.insert(crunch_base_data['Company_Information'])
-
-    # funding_document
-    res = requests.get('http://127.0.0.1:5000/funding')
-    funding = json.loads(res.text)
-    funding['Reference_id'] = reference_id
-    db.Funding.insert(funding)
-    res = requests.get('http://127.0.0.1:5000/print_data' + reference_id)
-    return json.loads(res.text)
-
+    # # Contact_person
+    # contact_info = contact_person(urls['Website'])
+    #
+    # # Company_detail
+    # company_info_ac = details(html_data['main'])
+    #
+    # # Company_Data
+    # db.Company_Data.insert({'Website': urls['Website'],
+    #                         "Company_Name": contact_info['companyName'],
+    #                         'Area_of_interest': company_info_ac['Area_of_interest']
+    #                         })
+    #
+    # # Get primary key for the document
+    # cursor = db.Company_Data.find({'Company_Name': contact_info['companyName']})
+    # reference_id = ''
+    # for cursor_data in cursor:
+    #     reference_id = str(cursor_data['_id'])
+    #     break
+    #
+    # # urls_document
+    # urls['Reference_id'] = reference_id
+    # db.URLs.insert(urls)
+    #
+    # # Contact_Person_document
+    # db.Contact_Person.insert({'First_Name': contact_info['firstName'],
+    #                           'Last_Name': contact_info['lastName'],
+    #                           'Position': contact_info['position'],
+    #                           'LinkedIn_Profile': contact_info['sourcePage'],
+    #                           'Email': contact_info['email'],
+    #                           'Reference_id': reference_id
+    #                           })
+    #
+    # # Jobs_document
+    # jobs(html_data['jobs'], reference_id)
+    #
+    # # Founders_document and Comp_info_document
+    # founder = Founder(html_data['crunch_base'], reference_id)
+    #
+    # # funding_document
+    # funding(html_data['funding'], reference_id)
+    #
+    #
+    # # print(angel)
+    # # print(founder)
+    # # print('IN TO PRINT')
+    # # print(info)
+    # # return json.dumps(info)
+    #
+    # return print_data(reference_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
